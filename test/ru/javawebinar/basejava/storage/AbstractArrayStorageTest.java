b7cd761b43e1801ca72cf4b2ecb076b2a50bb030
Igor Zinich <igor.zinich.it@gmail.com>
2022-03-28 20:10:33 +0300

    HW_4 test methods realization

--- test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
@@ -3,11 +3,18 @@ package ru.javawebinar.basejava.storage;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.function.ThrowingRunnable;
+import ru.javawebinar.basejava.exception.ExistStorageException;
 import ru.javawebinar.basejava.exception.NotExistStorageException;
+import ru.javawebinar.basejava.exception.StorageException;
 import ru.javawebinar.basejava.model.Resume;
 
 public abstract class AbstractArrayStorageTest {
     private Storage storage;
+
+    public AbstractArrayStorageTest(Storage storage) {
+        this.storage = storage;
+    }
 
     private static final String UUID_1 = "uuid1";
     private static final String UUID_2 = "uuid2";
@@ -23,22 +30,31 @@ public abstract class AbstractArrayStorageTest {
 
     @Test
     public void clear() {
+        storage.clear();
+        Assert.assertEquals(0, storage.size());
     }
 
     @Test
     public void update() {
+        storage.update(new Resume(UUID_1));
+
     }
 
     @Test
     public void save() {
+        storage.save(new Resume("Test_UUID"));
+        Assert.assertEquals("Test_UUID", storage.get("Test_UUID").getUuid());
     }
 
-    @Test
+    @Test(expected = NotExistStorageException.class)
     public void delete() {
+        storage.delete(UUID_2);
+        Assert.assertThrows(NotExistStorageException.class, (ThrowingRunnable) storage.get(UUID_2));
     }
 
     @Test
     public void getAll() {
+        Assert.assertEquals(new Resume[] {new Resume("uuid1"), new Resume("uuid2"), new Resume("uuid3")}, storage.getAll());
     }
 
     @Test
@@ -48,10 +64,28 @@ public abstract class AbstractArrayStorageTest {
 
     @Test
     public void get() {
+        Assert.assertEquals(new Resume("uuid1"), storage.get(UUID_1));
     }
 
     @Test(expected = NotExistStorageException.class)
     public void getNotExist() {
         storage.get("dummy");
+    }
+
+    @Test(expected = ExistStorageException.class)
+    public void getExist() {
+        storage.save(new Resume(UUID_1));
+    }
+
+    @Test(expected = StorageException.class)
+    public void getFullStorage() {
+        try {
+            for (int i = 4; i <= 10000; i++) {
+                storage.save(new Resume("uuid_" + i));
+            }
+        } catch (Exception e){
+            Assert.fail("Переполнение произошло раньше времени");
+        }
+        storage.save(new Resume("uuid_10001"));
     }
 }
