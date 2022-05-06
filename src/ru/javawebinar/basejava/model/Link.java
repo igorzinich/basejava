648d5e5382b55c2977c5bb3b99dace98a9132c0d
Igor Zinich <igor.zinich.it@gmail.com>
2022-05-06 23:49:33 +0300

    Lesson_10 json

--- src/ru/javawebinar/basejava/model/Link.java
+++ src/ru/javawebinar/basejava/model/Link.java
@@ -3,6 +3,7 @@ package ru.javawebinar.basejava.model;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import java.io.Serializable;
+import java.util.Objects;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Link implements Serializable {
@@ -30,5 +31,23 @@ public class Link implements Serializable {
     @Override
     public String toString() {
         return name + ": " + url;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Link link = (Link) o;
+
+        if (!name.equals(link.name)) return false;
+        return Objects.equals(url, link.url);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + (url != null ? url.hashCode() : 0);
+        return result;
     }
 }
