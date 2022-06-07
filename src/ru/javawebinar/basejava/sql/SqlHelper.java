9c819f7b3860c8273c10c0b5586cd2d8636b2f1b
Igor Zinich <igor.zinich.it@gmail.com>
2022-06-08 00:41:08 +0300

    Lesson_14 SqlTransaction realization

--- src/ru/javawebinar/basejava/sql/SqlHelper.java
+++ src/ru/javawebinar/basejava/sql/SqlHelper.java
@@ -1,7 +1,5 @@
 package ru.javawebinar.basejava.sql;
 
-import org.postgresql.util.PSQLException;
-import ru.javawebinar.basejava.exception.ExistStorageException;
 import ru.javawebinar.basejava.exception.StorageException;
 
 import java.sql.Connection;
@@ -15,6 +13,10 @@ public class SqlHelper {
         this.connectionFactory = connectionFactory;
     }
 
+    public void execute(String sql) {
+        execute(sql, PreparedStatement::execute);
+    }
+
     public <T> T execute(String sql, SQLExecutor<T> sqlExecutor) {
         try (Connection conn = connectionFactory.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
@@ -24,7 +26,19 @@ public class SqlHelper {
         }
     }
 
-    public void execute(String sql) {
-        execute(sql, PreparedStatement::execute);
+    public <T> T transactionalExecute(SqlTransaction<T> executor) {
+        try (Connection conn = connectionFactory.getConnection()) {
+            try {
+                conn.setAutoCommit(false);
+                T res = executor.execute(conn);
+                conn.commit();
+                return res;
+            } catch (SQLException e) {
+                conn.rollback();
+                throw ExceptionUtil.convertException(e);
+            }
+        } catch (SQLException e) {
+            throw new StorageException(e);
+        }
     }
 }
